8b53a17beeffcec8eacf8d734ced894c7a4bd891
Kavin Valli <kavinvalli@gmail.com>
2021-12-02 10:19:56 +0530

    feat(neovim): Lua lsp formatting on save

--- nvim/.config/nvim/lua/k/autocmd.lua
+++ nvim/.config/nvim/lua/k/autocmd.lua
@@ -1,7 +1,10 @@
 require('k.utils').create_augroups {
   fmt = {
     {
-      'BufWritePre * undojoin | Neoformat'
-    },
+      'BufWritePre *.{js,jsx,ts,tsx} undojoin | Neoformat',
+      'BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)'
+    }
   }
 }
+
+vim.cmd [[ autocmd BufNewFile,BufRead *.tsx,*.jsx set filetype=typescript.tsx ]]
