fb1b9f56123097efe36333c4368fd204bd627d4f
Kavin Valli <kavinvalli@gmail.com>
2021-12-07 17:26:19 +0530

    fix(neovim): format on save with Neoformat

--- nvim/.config/nvim/lua/k/autocmd.lua
+++ nvim/.config/nvim/lua/k/autocmd.lua
@@ -1,8 +1,8 @@
 require('k.utils').create_augroups {
   fmt = {
     {
-      'BufWritePre *.{js,jsx,ts,tsx} undojoin | Neoformat',
-      'BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)'
+      'BufWritePre * Neoformat'
+      -- 'BufWritePre *.lua lua vim.lsp.buf.formatting_sync(nil, 100)'
     }
   }
 }
