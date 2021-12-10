ad955b744512c2e4490ad1cfc295ff641df81249
Kavin Valli <kavinvalli@gmail.com>
2021-12-10 12:01:08 +0530

    chore(neovim): C-j C-k for tab completion in coc

--- nvim/.config/nvim/lua/k/plugins/coc.lua
+++ nvim/.config/nvim/lua/k/plugins/coc.lua
@@ -26,8 +26,8 @@ vim.api.nvim_set_keymap("n", "K", ":call CocActionAsync('doHover')<cr>",
 vim.api.nvim_set_keymap("i", "<C-space>", "coc#refresh()",
                         {noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<C-Space>", "coc#refresh()", { silent = true, expr = true })
-vim.api.nvim_set_keymap("i", "<TAB>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
-vim.api.nvim_set_keymap("i", "<S-TAB>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
+vim.api.nvim_set_keymap("i", "<C-j>", "pumvisible() ? '<C-n>' : '<TAB>'", {noremap = true, silent = true, expr = true})
+vim.api.nvim_set_keymap("i", "<C-k>", "pumvisible() ? '<C-p>' : '<C-h>'", {noremap = true, expr = true})
 vim.api.nvim_set_keymap("i", "<CR>", "pumvisible() ? coc#_select_confirm() : '<C-G>u<CR><C-R>=coc#on_enter()<CR>'", {silent = true, expr = true, noremap = true})
 
 -- Indentation
