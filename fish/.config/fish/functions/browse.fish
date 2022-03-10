9e9d815a93b9c0f7c1567dfcb509fb7712254fe2
Kavin Valli <mail@kavin.me>
2022-03-10 20:09:12 +0530

    chore: change browse fish function from google to duckduckgo

--- fish/.config/fish/functions/browse.fish
+++ fish/.config/fish/functions/browse.fish
@@ -1,6 +1,6 @@
 function browse
   echo $argv[1]
-  set search "https://google.com/search?q="$argv[1]
+  set search "https://duckduckgo.com/?q="$argv[1]
   if test (count $argv) -gt 1
     for x in $argv[2..(count $argv)]
       echo $x
